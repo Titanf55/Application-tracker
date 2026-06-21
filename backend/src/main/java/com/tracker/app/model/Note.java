7d60f27a005768d00d34612967019aefe01b3f9f
Titanf55 <kashishrajput0318@gmail.com>
2026-06-22 00:34:41 +0530

    Note.java created

---
+++ backend/src/main/java/com/tracker/app/model/Note.java
@@ -0,0 +1,65 @@
+package com.tracker.app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_id", nullable = false)
+    private Application application;
+
+    @NotBlank
+    private String type;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    @NotBlank
+    private String content;
+
+    public Note() {}
+
+    public Note(Application application, String type, String content) {
+        this.application = application;
+        this.type = type;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Application getApplication() {
+        return application;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
